87ad5f9562c940078cb7cd84b7e07af70651798b
Jeffta <to.lfj37@gmail.com>
2023-10-09 10:16:48 +0200

    Initial commit

---
+++ lib/config.php
@@ -0,0 +1,9 @@
+<?php
+define("_ARTICLES_IMAGES_FOLDER_", "/uploads/articles/");
+define("_ASSETS_IMAGES_FOLDER_", "/assets/images/");
+define("_ADMIN_ITEM_PER_PAGE_", 10);
+define("_DB_NAME_", "techtrendz");
+define("_DB_USER_", "root");
+define("_DB_PASSWORD_", "admin");
+define("_APP_EMAIL_", "xxx@gmail.com");
+
