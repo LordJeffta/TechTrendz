c143fb424f1867d470afc5bf2cabd46770711ece
Jeffta <to.lfj37@gmail.com>
2023-10-09 13:53:49 +0200

    Add create account

--- lib/user.php
+++ lib/user.php
@@ -1,11 +1,16 @@
 <?php
 
-function addUser(PDO $pdo, string $first_name, string $last_name, string $email, string $password, $role = "user")
+function addUser(PDO $pdo, string $first_name, string $last_name, string $email, string $password, $role = "user"): bool
 {
-    /*
-        @todo faire la requête d'insertion d'utilisateur et retourner $query->execute();
-        Attention faire une requête préparer et à binder les paramètres
-    */
+    $query = $pdo->prepare("INSERT INTO techtrendz.users (email, password, first_name, last_name, role)
+                        VALUES (:email, :password, :first_name, :last_name, :role);");
+    $query->bindValue(":first_name", $first_name, PDO::PARAM_STR_CHAR);
+    $query->bindValue(":last_name", $last_name, PDO::PARAM_STR_CHAR);
+    $query->bindValue(":email", $email, PDO::PARAM_STR_CHAR);
+    $query->bindValue(":password", $password, PDO::PARAM_STR_CHAR);
+    $query->bindValue(":role", $role, PDO::PARAM_STR_CHAR);
+    error_log($query->queryString);
+    return $query->execute();
 }
 
 function verifyUserLoginPassword(PDO $pdo, string $email, string $password)
@@ -14,8 +19,7 @@ function verifyUserLoginPassword(PDO $pdo, string $email, string $password)
         @todo faire une requête qui récupère l'utilisateur par email et stocker le résultat dans user
         Attention faire une requête préparer et à binder les paramètres
     */
-    
- 
+
 
     /*
         @todo Si on a un utilisateur et que le mot de passe correspond (voir fonction  native password_verify)
